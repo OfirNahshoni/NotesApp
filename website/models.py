b83fafefc0cdf02d7c2fac48fcde38200818ae20
Ofir Nahshoni <58309185+OfirNahshoni@users.noreply.github.com>
2022-09-20 00:10:29 +0300

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
@@ -28,44 +27,3 @@ class Note(db.Model):
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # Foreign key
-    
-=======
-<<<<<<< HEAD
-from . import db
-from flask_login import UserMixin
-from sqlalchemy.sql import func
-
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    full_name = db.Column(db.String(100))
-=======
-from . import db
-from flask_login import UserMixin
-from sqlalchemy.sql import func
-
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    full_name = db.Column(db.String(100))
->>>>>>> 2c0eaf93d5e8dfa9a21d2d9aca443cea47fb8e23
-    notes = db.relationship('Note')
->>>>>>> 2571ab1e679215ecbdacf533417c5cfa59dd8650
